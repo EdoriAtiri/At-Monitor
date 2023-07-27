0b23c90afa3e4a2f114ee1a132757b952b6cbd25
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-07-27 21:47:30 +0100

    BE: Add registerUser method to user Contr

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,60 @@
+const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const User = require('../models/userModel')
+
+// @desc Register a new user
+// @route /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { firstName, lastName, email, password } = req.body
+
+  // Validation
+  if (!firstName || !lastName || !email || !password) {
+    req.statusCode(400)
+    throw new Error('Please include all required fields')
+  }
+
+  // Check if user exists
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  if (password.length < 8) {
+    res.status(400)
+    throw new Error('Password must be at least 8 characters')
+  }
+
+  // Hash Password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  // Create user
+  const user = await User.create({
+    firstName,
+    lastName,
+    email,
+    password: hashedPassword,
+  })
+
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
+})
+
+module.exports = {
+  registerUser,
+}
