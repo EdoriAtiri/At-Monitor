6a235d9a3c62b9ffa68bed9b5c5fd258767c4b82
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-07-27 16:16:03 +0100

    BE: Add UserModel

---
+++ Backend/models/userModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, 'Please add a first name'],
+    },
+    lastName: {
+      type: String,
+      required: [true, 'Please add a last name'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please add an Email'],
+    },
+    password: {
+      type: String,
+      required: [true, 'Please add a password'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model('User', userSchema)
