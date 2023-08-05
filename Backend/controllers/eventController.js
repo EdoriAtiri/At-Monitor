c30acd1a8ff2b204b05068f6e8be6ba39cf9d122
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-08-05 09:32:59 +0100

    BE: Add updateEvent to eventController

--- Backend/controllers/eventController.js
+++ Backend/controllers/eventController.js
@@ -5,11 +5,12 @@ const Event = require('../models/EventModel')
 
 // @desc Create a new event
 // @route /api/events
-// @access Public
+// @access Private
 const createEvent = asyncHandler(async (req, res) => {
   // Get Admin using the Id in the jwt
-  const { eventName, eventDate, description, linkId } = req.body
   const admin = await Admin.findById(req.admin.id)
+
+  const { eventName, eventDate, description, linkId } = req.body
 
   if (!eventName || !eventDate) {
     res.status(400)
@@ -32,6 +33,38 @@ const createEvent = asyncHandler(async (req, res) => {
   res.status(201).json(event)
 })
 
+// @desc Update an event
+// @route /api/events/:id
+// @access Private
+const updateEvent = asyncHandler(async (req, res) => {
+  // Get Admin using the Id in the jwt
+  const admin = await Admin.findById(req.admin.id)
+
+  if (!admin) {
+    res.status(401)
+    throw new Error('Admin not found')
+  }
+
+  const eventId = await Event.findById(req.params.id)
+
+  if (!eventId) {
+    res.status(404)
+    throw new Error('Event not found')
+  }
+
+  //   if (event.admin !== admin) {
+  //     res.status(404)
+  //     throw new Error('Not Authorized')
+  //   }
+
+  const updatedEvent = await Event.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  })
+
+  res.status(201).json(updatedEvent)
+})
+
 module.exports = {
   createEvent,
+  updateEvent,
 }
