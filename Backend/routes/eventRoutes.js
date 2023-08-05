a437e780b029dd8da9351036785ab3446cb24d93
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-08-05 09:46:34 +0100

    BE: Add route for updateEvent

--- Backend/routes/eventRoutes.js
+++ Backend/routes/eventRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const { protect } = require('../middleware/authMiddleware')
 
-const { createEvent } = require('../controllers/eventController')
+const { createEvent, updateEvent } = require('../controllers/eventController')
 
 router.route('/').post(protect, createEvent)
+
+router.route('/:id').patch(protect, updateEvent)
 
 module.exports = router
