e49dc4f3b3b21e756ca5cf886a6db3682e87258e
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-08-17 11:51:11 +0100

    FE: Add login page structure

--- frontend/src/app/components/Signup.jsx
+++ frontend/src/app/components/Signup.jsx
@@ -1,7 +1,91 @@
+import { useState } from 'react'
+
 function Signup() {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    password2: '',
+  })
+
+  const { firstName, lastName, email, password, password2 } = formData
+
+  const onChange = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
   return (
     <div className="w-72 h-96 bg-blue-500 rounded-lg px-6">
       <h2>Administrator Signup</h2>
+      <p>Hey, Enter your details to signup for an account</p>
+
+      <form>
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            id="firstName"
+            name="firstName"
+            value={firstName}
+            onChange={onChange}
+            placeholder="Enter your first name"
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            id="lastName"
+            name="lastName"
+            value={lastName}
+            onChange={onChange}
+            placeholder="Enter your last name"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            value={email}
+            onChange={onChange}
+            placeholder="Enter your email address"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            name="password"
+            value={password}
+            onChange={onChange}
+            placeholder="Enter password"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            className="form-control"
+            id="password2"
+            name="password2"
+            value={password2}
+            onChange={onChange}
+            placeholder="Confirm password"
+            required
+          />
+        </div>
+      </form>
     </div>
   )
 }
