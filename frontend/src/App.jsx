bc03a4eac49e424aa0485d2dbd4f38a2ec299f6c
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-08-17 11:20:56 +0100

    FE: Add Login and Register Components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './app/pages/Login.jsx'
+
 function App() {
   return (
     <>
-      <h2 className="text-red-700 text-6xl">ok</h2>
+      <Router>
+        <div className="app">
+          <Routes>
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   )
 }
