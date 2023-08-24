5e3450638feb2afeeec5e7c57ab2f2624c7bd1f4
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-08-24 19:54:28 +0100

    FE: Add link to signup and login to home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './app/pages/Home.jsx'
 import Signup from './app/pages/Signup.jsx'
+import Login from './app/pages/Login.jsx'
 
 function App() {
   return (
     <>
       <Router>
-        <div className="app min-h-screen">
+        <div className="w-full min-h-screen">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </div>
       </Router>
--- frontend/src/app/pages/Home.jsx
+++ frontend/src/app/pages/Home.jsx
@@ -1,8 +1,9 @@
+import { Link } from 'react-router-dom'
 import Button from '../components/Button'
 
 function Home() {
   return (
-    <div className="w-100 flex flex-row ">
+    <div className="w-full min-h-screen flex flex-row">
       <div className="w-1/2 py-10 space-y-32">
         <div className="font-bold text-2xl ml-10">
           <span className="text-3xl">A</span>
@@ -17,16 +18,25 @@ function Home() {
             provident, deleniti ipsum molestias illo aut!
           </p>
           <div className="flex flex-row space-x-4 lg:space-x-6 xl:space-x-8">
-            <Button text="Sign Up" />
-            <Button text="Login" />
+            <Link to="/signup">
+              <Button text="Sign Up" />
+            </Link>
+            <Link to="/login">
+              <Button text="Login" />
+            </Link>
           </div>
         </div>
       </div>
 
-      <div className="w-1/2">
-        <ul>
-          <li>About</li>
+      <div className="w-1/2 py-10 px-10 flex flex-col relative">
+        <ul className="text-sm h-fit py-4 xl:text-base flex flex-row gap-6 justify-end w-full">
+          <li className="nav">About</li>
+          <li className="nav">Contact</li>
         </ul>
+        <div className="font-bold text-7xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <span className="text-8xl xl">A</span>
+          <span className="-ml-4">M</span>
+        </div>
       </div>
     </div>
   )
