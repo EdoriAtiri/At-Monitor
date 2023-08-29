8a58c7ab0f2e5089d2a4cf5511b65070dae54ff9
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-08-29 14:22:53 +0100

    FE: Remove NotLoggedIn component - redundant

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './app/pages/Home.jsx'
 import Signup from './app/pages/Signup.jsx'
 import Login from './app/pages/Login.jsx'
+// import PrivateRoute from './app/components/PrivateRoute.jsx'
 
 function App() {
   return (
