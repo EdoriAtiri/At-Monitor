403c81ff611d901d48322d2417354ca57fc76eac
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-08-29 14:07:32 +0100

    FE: Add useAuthStatus hook to check login state

---
+++ frontend/src/app/hooks/useAuthStatus.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+
+export const useAuthStatus = () => {
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [checkingStatus, setCheckingStatus] = useState(true)
+
+  const { admin } = useSelector((state) => state.auth)
+
+  useEffect(() => {
+    if (admin) {
+      setLoggedIn(true)
+    } else {
+      setLoggedIn(false)
+    }
+
+    setCheckingStatus(false)
+  }, [admin])
+
+  return { loggedIn, checkingStatus }
+}
--- frontend/src/app/pages/Home.jsx
+++ frontend/src/app/pages/Home.jsx
@@ -1,43 +1,7 @@
-import { Link } from 'react-router-dom'
-import Button from '../components/Button'
-import Logo from '../components/Logo'
+import NotLoggedIn from './NotLoggedIn'
 
 function Home() {
-  return (
-    <div className="w-full min-h-screen flex flex-row">
-      <div className="w-1/2 py-10 space-y-32">
-        <Logo />
-        <div className="px-10 xl:px-20 m-auto space-y-6">
-          <h1 className="font-bold text-4xl lg:text-6xl">
-            Track Event Attendance{' '}
-          </h1>
-          <p className="text-sm lg:text-lg">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur ad
-            provident, deleniti ipsum molestias illo aut!
-          </p>
-          <div className="flex flex-row space-x-4 lg:space-x-6 xl:space-x-8">
-            <Link to="/signup">
-              <Button text="Sign Up" />
-            </Link>
-            <Link to="/login">
-              <Button text="Login" />
-            </Link>
-          </div>
-        </div>
-      </div>
-
-      <div className="w-1/2 py-10 px-10 flex flex-col relative">
-        <ul className="text-sm h-fit py-4 xl:text-base flex flex-row gap-6 justify-end w-full">
-          <li className="nav">About</li>
-          <li className="nav">Contact</li>
-        </ul>
-        <div className="font-bold text-7xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          <span className="text-8xl xl">A</span>
-          <span className="-ml-4">M</span>
-        </div>
-      </div>
-    </div>
-  )
+  return <NotLoggedIn />
 }
 
 export default Home
