d21c8e0c8d60006f8aa4287a05ffb84234c9df26
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-08-29 14:19:20 +0100

    FE: Add PrivateRoute Component

--- frontend/src/app/components/Logo.jsx
+++ frontend/src/app/components/Logo.jsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom'
+
 function Logo() {
   return (
-    <div className="font-bold text-2xl ml-10 pt-10 absolute top-0 left-0">
-      <span className="text-3xl">A</span>
-      <span className="-ml-2">M</span>
-    </div>
+    <Link to="/">
+      <div className="font-bold text-2xl ml-10 pt-10 absolute top-0 left-0">
+        <span className="text-3xl">A</span>
+        <span className="-ml-2">M</span>
+      </div>
+    </Link>
   )
 }
 
--- frontend/src/app/pages/Home.jsx
+++ frontend/src/app/pages/Home.jsx
@@ -1,7 +1,10 @@
-import NotLoggedIn from './NotLoggedIn'
+import { useAuthStatus } from '../hooks/useAuthStatus'
+import NotLoggedIn from '../components/NotLoggedIn'
 
 function Home() {
-  return <NotLoggedIn />
+  const { loggedIn } = useAuthStatus()
+
+  return loggedIn ? <div>raa</div> : <NotLoggedIn />
 }
 
 export default Home
