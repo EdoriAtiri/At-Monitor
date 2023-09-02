955043071ce521cf29fb0dfa4594baa738a9b3bd
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-09-02 22:09:02 +0100

    FE: Dispatch getEvents from Events page

--- frontend/src/app/features/Events/eventSlice.js
+++ frontend/src/app/features/Events/eventSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import eventService from './EventService'
 
 const initialState = {
   myEvents: [],
--- frontend/src/app/pages/Events.jsx
+++ frontend/src/app/pages/Events.jsx
@@ -1,6 +1,25 @@
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { getEvents, reset } from '../features/Events/eventSlice'
 import EventCard from '../components/EventCard'
 
 function Events() {
+  const { myEvents, isSuccess } = useSelector((state) => state.myEvents)
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    return () => {
+      if (isSuccess) {
+        dispatch(reset())
+      }
+    }
+  }, [dispatch, isSuccess])
+
+  useEffect(() => {
+    dispatch(getEvents())
+  }, [dispatch])
+
   return (
     <div className="w-full mx-6 mt-10 mb-6">
       <heading className="flex items-center justify-between text-xl font-semibold">
