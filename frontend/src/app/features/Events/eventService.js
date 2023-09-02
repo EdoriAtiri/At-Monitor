f7a278b16b74a78eb56563718d3d8d5e404c31ab
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-09-02 21:45:22 +0100

    FE: Add getEvents method to eventService

--- frontend/src/app/features/Events/eventService.js
+++ frontend/src/app/features/Events/eventService.js
@@ -1,3 +1,22 @@
 import axios from 'axios'
 
 const API_URL = 'http://localhost:5000/api/events/'
+
+// Get admin events
+const getEvents = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }
+
+  const response = await axios.get(API_URL, config)
+
+  return response.data
+}
+
+const eventService = {
+  getEvents,
+}
+
+export default eventService
