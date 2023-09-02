24b3053460b8cc9ad379759b4c689c95d9dfedc5
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-09-02 21:39:28 +0100

    FE: Add eventReducer to store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../app/features/Auth/authSlice'
+import eventReducer from '../app/features/Events/eventSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    myEvents: eventReducer,
   },
 })
