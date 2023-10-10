4b568f6a5d5d71f69b62782a3dce82312bdf4aec
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-10-10 14:00:47 +0100

    FE: Add extrareducers for grtRegistrars

--- frontend/src/app/features/Registrars/registrarSlice.js
+++ frontend/src/app/features/Registrars/registrarSlice.js
@@ -9,3 +9,54 @@ const initialState = {
   isLoading: false,
   message: '',
 }
+
+// Get registrars
+export const getRegistrars = createAsyncThunk(
+  'registrars/getAll',
+  async (_, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.admin.token
+      return await registrarService.getRegistrars(token)
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString()
+
+      console.log(message)
+
+      return thunkAPI.rejectWithValue(message)
+    }
+  }
+)
+
+export const registrarSlice = createSlice({
+  name: 'registrars',
+  initialState,
+  reducers: {
+    // eslint-disable-next-line no-unused-vars
+    reset: (state) => initialState,
+    // resetSuccess: (state) => (state.isSuccess = false),
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getRegistrars.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(getRegistrars.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.registrars = action.payload
+      })
+      .addCase(getRegistrars.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+      })
+  },
+})
+
+export const { reset, resetSuccess } = registrarSlice.actions
+export default registrarSlice.reducer
