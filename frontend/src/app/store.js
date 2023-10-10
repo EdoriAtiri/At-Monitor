0d66af052e462782ae7eec2a15492c2a817eeb5c
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-10-10 14:04:04 +0100

    FE: Add Registrar to redux store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../app/features/Auth/authSlice'
 import eventReducer from '../app/features/Events/eventSlice'
+import registrarReducer from '../app/features/Registrars/registrarSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     myEvents: eventReducer,
+    registrar: registrarReducer,
   },
 })
