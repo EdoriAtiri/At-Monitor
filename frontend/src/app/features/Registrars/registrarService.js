7a06e6c0a628ca4a6d32a0c3f32a54942ebf2dfa
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-10-10 14:02:30 +0100

    FE: Add getRegistrar service

--- frontend/src/app/features/Registrars/registrarService.js
+++ frontend/src/app/features/Registrars/registrarService.js
@@ -1,3 +1,21 @@
-// import axios from 'axios'
+import axios from 'axios'
 
-// const API_URL = 'http://localhost:5000/api/registrars/'
+const API_URL = 'http://localhost:5000/api/registrars/'
+
+const getRegistrars = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }
+
+  const response = await axios.get(API_URL, config)
+
+  return response.data
+}
+
+const registrarService = {
+  getRegistrars,
+}
+
+export default registrarService
