dd245fb92c0030091ce37d2f3747c769c90100cd
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-10-13 16:16:14 +0100

    FE: Add Registrar Stats

--- frontend/src/app/pages/Registrars.jsx
+++ frontend/src/app/pages/Registrars.jsx
@@ -1,4 +1,51 @@
+import { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { getRegistrars, reset } from '../features/Registrars/registrarSlice'
+
 const Registrars = () => {
+  // eslint-disable-next-line no-unused-vars
+  const { registrars, isSuccess, isLoading } = useSelector(
+    (state) => state.registrars
+  )
+  const [registrarStats, setRegistrarStats] = useState({
+    total: '',
+    active: '',
+    inactive: '',
+  })
+
+  const { total, active, inactive } = registrarStats
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    return () => {
+      if (isSuccess) {
+        dispatch(reset())
+      }
+    }
+  }, [dispatch, isSuccess])
+
+  // Gets Registrars on mount
+  useEffect(() => {
+    dispatch(getRegistrars())
+  }, [dispatch])
+
+  // Creates stats when getRegistrars is successful
+  useEffect(() => {
+    if (Array.isArray(registrars)) {
+      setRegistrarStats({
+        total: registrars.length || 0,
+        active:
+          registrars.filter((registrar) => registrar.isActivated).length || 0,
+        inactive:
+          registrars.filter((registrar) => !registrar.isActivated).length || 0,
+      })
+    }
+  }, [registrars])
+
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
+
   return (
     <div className="mx-6 mt-10 mb-6">
       {' '}
@@ -16,17 +63,17 @@ const Registrars = () => {
         <div className="stats shadow">
           <div className="stat">
             <div className="stat-title">Total Registrars</div>
-            <div className="stat-value text-2xl">{}</div>
+            <div className="stat-value text-2xl">{total}</div>
           </div>
 
           <div className="stat">
             <div className="stat-title">Active</div>
-            <div className="stat-value text-2xl">{}</div>
+            <div className="stat-value text-2xl">{active}</div>
           </div>
 
           <div className="stat">
             <div className="stat-title">Inactive</div>
-            <div className="stat-value text-2xl">{}</div>
+            <div className="stat-value text-2xl">{inactive}</div>
           </div>
         </div>
       </div>
--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -7,6 +7,6 @@ export const store = configureStore({
   reducer: {
     auth: authReducer,
     myEvents: eventReducer,
-    registrar: registrarReducer,
+    registrars: registrarReducer,
   },
 })
