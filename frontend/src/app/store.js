45513601fc5f6e2fef5b2aa28368568a93f46470
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-11-13 17:38:02 +0100

    FE: add memberReducer to redux store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../app/features/Auth/authSlice'
 import eventReducer from '../app/features/Events/eventSlice'
 import registrarReducer from '../app/features/Registrars/registrarSlice'
+import memberReducer from '../app/features/Members/memberSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     myEvents: eventReducer,
     registrars: registrarReducer,
+    members: memberReducer,
   },
 })
