d6db8826b4fdcbac5b2e0b8053ca6d94d6d1ba25
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-11-14 14:27:36 +0100

    FE: Add getMember fn to memberService

--- frontend/src/app/features/Members/memberService.jsx
+++ frontend/src/app/features/Members/memberService.jsx
@@ -14,8 +14,21 @@ const getMembers = async (token) => {
   return response.data
 }
 
+// Get an admin member
+const getMember = async (id, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }
+
+  const response = await axios.get(API_URL + id, config)
+  return response.data
+}
+
 const memberService = {
   getMembers,
+  getMember,
 }
 
 export default memberService
