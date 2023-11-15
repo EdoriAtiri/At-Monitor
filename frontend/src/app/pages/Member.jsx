eb9e1e7773ebf833bd5f5ec07ed97f1f532e3706
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-11-15 16:48:54 +0100

    FE: add header to MemberForm

--- frontend/src/app/pages/Member.jsx
+++ frontend/src/app/pages/Member.jsx
@@ -1,14 +1,15 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import { getMember } from "../features/Members/memberSlice";
+import { getMember, reset } from "../features/Members/memberSlice";
 
 const Member = () => {
   const { member, isLoading, isError, message, isSuccess } = useSelector(
     (state) => state.members,
   );
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const { memberId } = useParams();
 
@@ -18,16 +19,32 @@ const Member = () => {
     }
 
     dispatch(getMember(memberId));
-    console.log(member, isLoading, isSuccess);
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isError, message, memberId]);
 
   return (
     <div className="mx-6 mb-6 mt-10">
-      <h1 className="mb-5 text-3xl uppercase">
-        {member?.fullName?.split(" ")[0]}
-      </h1>
+      <header className="mb-5 flex justify-between">
+        <h1 className=" text-3xl uppercase">
+          {member?.fullName?.split(" ")[0]}
+        </h1>
+        {/* Actions */}
+        <div className="flex gap-3">
+          <button
+            onClick={() => {
+              dispatch(reset()),
+                navigate(`/dashboard/members/create?type=edit&id=${memberId}`);
+            }}
+            className="rounded-md border border-gray-700 p-1 text-lg"
+          >
+            Edit
+          </button>
+          <button className="rounded-md border border-gray-700 p-1 text-lg">
+            Delete
+          </button>
+        </div>
+      </header>
 
       <table className="flex w-full justify-evenly rounded-md px-4 py-2 lg:w-96 lg:p-4">
         <thead>
