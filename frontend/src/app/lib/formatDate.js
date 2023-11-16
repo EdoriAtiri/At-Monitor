d0ebf62d1b267652d4e92321aac1422c5b6573e4
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-11-16 19:50:08 +0100

    FE: add new formatDate fn to formatDate.js

--- frontend/src/app/lib/formatDate.js
+++ frontend/src/app/lib/formatDate.js
@@ -2,3 +2,9 @@ export const formatDateDisplay = (inputDate) => {
   const options = { year: "numeric", month: "short", day: "numeric" };
   return new Date(inputDate).toLocaleDateString("en-GB", options);
 };
+
+export const formatDate = (dateString) => {
+  if (!dateString) return "";
+  const date = new Date(dateString);
+  return date.toISOString().split("T")[0]; // Extract yyyy-mm-dd
+};
