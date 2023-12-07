c6d3bbe82dc13f9373abc85421025b8c23f4d28b
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-12-07 15:35:59 +0100

    FE: add state to manage registration form

--- frontend/src/app/components/EventRegister.jsx
+++ frontend/src/app/components/EventRegister.jsx
@@ -1,8 +1,26 @@
 import { useSelector, useDispatch } from "react-redux";
 import { FaPlus } from "react-icons/fa6";
+import { useState } from "react";
+// import { FaMale } from "react-icons/fa";
 
 const EventRegister = () => {
+  const [isRegistrationForm, setIsRegistrationForm] = useState(false);
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    phone: "",
+    firstTimer: "",
+    gender: "",
+    memberId: "",
+  });
+  const { fullName, email, phone, firstTimer, gender } = formData;
   const { myEvent } = useSelector((state) => state.myEvents);
+
+  const onChange = () => {};
+  const onSubmit = () => {};
+  const closeForm = () => {
+    setIsRegistrationForm(false);
+  };
   return (
     // Actions
     <div className="mx-auto max-w-4xl">
@@ -10,7 +28,10 @@ const EventRegister = () => {
         <div>
           <h4 className="text-xl font-bold text-gray-800"> Registration</h4>
         </div>
-        <button className="grid h-8 w-8 place-content-center rounded-full bg-sky-400 transition-all hover:bg-slate-400 focus:border active:scale-95">
+        <button
+          onClick={() => setIsRegistrationForm(true)}
+          className="grid h-8 w-8 place-content-center rounded-full bg-sky-400 transition-all hover:bg-slate-400 focus:border active:scale-95"
+        >
           <FaPlus />
         </button>{" "}
       </div>
@@ -54,6 +75,132 @@ const EventRegister = () => {
           </tbody>
         </table>
       </div>
+
+      {/* update register */}
+      {isRegistrationForm && (
+        <div className="absolute left-0 top-0 z-30  h-screen w-screen">
+          <button
+            onClick={closeForm}
+            className=" absolute left-0 top-0 h-full w-full bg-black opacity-30"
+          ></button>
+
+          <div className="absolute left-1/2 top-1/2  h-fit w-80 -translate-x-1/2 -translate-y-1/2 transform rounded-2xl bg-white px-6 py-6">
+            <button
+              onClick={closeForm}
+              className="absolute right-0 top-0 p-1 pr-2 text-lg font-bold uppercase"
+            >
+              x
+            </button>
+
+            {/* Registration Form */}
+            <form
+              onSubmit={onSubmit}
+              className="h-full w-full  space-y-7 [&>*]:w-full"
+            >
+              <div className="space-y-4">
+                {/*  */}
+                <div className="space-y-2">
+                  <label
+                    htmlFor="fullName"
+                    className="text-sm md:text-base lg:text-xl"
+                  >
+                    Full Name
+                  </label>
+                  <input
+                    type="text"
+                    className="form-input-style"
+                    id="fullName"
+                    name="fullName"
+                    value={fullName}
+                    onChange={onChange}
+                    required
+                  />
+                </div>
+
+                <div className="space-y-2">
+                  <label
+                    htmlFor="email"
+                    className="text-sm md:text-base lg:text-xl"
+                  >
+                    email
+                  </label>
+                  <input
+                    type="email"
+                    className="form-input-style"
+                    id="email"
+                    name="email"
+                    value={email}
+                    onChange={onChange}
+                    required
+                  />
+                </div>
+
+                <div className="space-y-2">
+                  <label
+                    htmlFor="phone"
+                    className="text-sm md:text-base lg:text-xl"
+                  >
+                    Phone No
+                  </label>
+                  <input
+                    type="tel"
+                    className="form-input-style"
+                    id="phone"
+                    name="phone"
+                    value={phone}
+                    onChange={onChange}
+                    required
+                  />
+                </div>
+
+                <div className="space-y-2">
+                  <label
+                    htmlFor="firstTimer"
+                    className="text-sm md:text-base lg:text-xl"
+                  >
+                    Is this your first time?
+                  </label>
+                  <select
+                    onChange={onChange}
+                    className="form-input-style py-1 pr-2"
+                    name="firstTimer"
+                    id="firstTimer"
+                    value={firstTimer}
+                  >
+                    <option value={true}>Yes</option>
+                    <option value={false}>No</option>
+                  </select>
+                </div>
+
+                <div className="space-y-2">
+                  <label
+                    htmlFor="gender"
+                    className="text-sm md:text-base lg:text-xl"
+                  >
+                    Is this your first time?
+                  </label>
+                  <select
+                    onChange={onChange}
+                    className="form-input-style py-1 pr-2"
+                    name="gender"
+                    id="gender"
+                    value={gender}
+                  >
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                  </select>
+                </div>
+
+                <div className="btn mt-36">
+                  <button className="form-input-style grid place-content-center bg-black px-2 font-bold text-white transition-transform active:scale-95">
+                    Register
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
