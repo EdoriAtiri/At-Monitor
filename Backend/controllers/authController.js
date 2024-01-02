d07c49b05e6b37b924f78c418bc4335b129f2b82
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2024-01-02 08:08:42 +0100

    BE: add authController for admin/reg login

---
+++ Backend/controllers/authController.js
@@ -0,0 +1,28 @@
+const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
+const generateToken = require('../lib/genToken')
+
+const Admin = require('../models/adminModel')
+
+// @desc Login a new admin
+// @route /api/login
+// @access Public
+const loginAdmin = asyncHandler(async (req, res) => {
+  const { email, password } = req.body
+
+  const admin = await Admin.findOne({ email })
+
+  //  Check if admin and password match
+  if (admin && (await bcrypt.compare(password, admin.password))) {
+    res.status(200).json({
+      _id: admin._id,
+      firstName: admin.firstName,
+      lastName: admin.lastName,
+      email: admin.email,
+      token: generateToken(admin._id, '30d'),
+    })
+  } else {
+    res.status(401)
+    throw new Error('Invalid Credentials')
+  }
+})
