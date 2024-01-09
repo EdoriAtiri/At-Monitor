a691ef9f60e922668859d15e7c11a3c25c927292
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2024-01-09 16:06:00 +0100

    BE: Add editRegistrar to adminRoutes

--- Backend/routes/adminRoutes.js
+++ Backend/routes/adminRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
-const { registerAdmin, loginAdmin } = require('../controllers/adminController')
+const {
+  registerAdmin,
+  loginAdmin,
+  editAdmin,
+} = require('../controllers/adminController')
+const { protect } = require('../middleware/authMiddleware')
 
 router.post('/', registerAdmin)
+router.patch('/', protect, editAdmin)
 
 router.post('/login', loginAdmin)
 
