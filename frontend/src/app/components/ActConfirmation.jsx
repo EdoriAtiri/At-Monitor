614f06a97404f4a86535c5261af846a52be5fbed
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2024-01-22 13:45:53 +0100

    FE: refactor actConfirmation card

--- frontend/src/app/components/ActConfirmation.jsx
+++ frontend/src/app/components/ActConfirmation.jsx
@@ -2,12 +2,13 @@ import PropTypes from "prop-types";
 
 const ActConfirmation = ({ title, action, onClickBtn, onClickCancel }) => {
   return (
-    <div className="absolute left-0 top-0 h-screen w-full">
+    <div className="absolute left-0 top-0 h-screen  w-full">
       <button
         onClick={onClickCancel}
         className="absolute left-0 top-0 z-10 h-screen w-full bg-black opacity-30"
       ></button>
-      <div className="card absolute left-1/2 top-1/2 z-20 w-96 -translate-x-1/2 -translate-y-1/2 transform bg-gray-100 shadow-2xl">
+
+      <div className="card absolute left-1/2 top-1/2 z-20 w-80 -translate-x-1/2 -translate-y-1/2 transform bg-gray-100 shadow-2xl sm:w-96">
         <div className="card-body">
           <h2 className="card-title capitalize">{title}</h2>
           <p className="mb-4 font-semibold">
