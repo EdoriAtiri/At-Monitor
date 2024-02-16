5af0ce0d2f067ad1ced9d2928048bd722cc1431b
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2024-02-16 19:51:12 +0100

    FE: refactor baseurl in eventService

--- frontend/src/app/features/Events/eventService.js
+++ frontend/src/app/features/Events/eventService.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-
-const API_URL = "http://localhost:5000/api/events/";
+const API_URL = import.meta.env.VITE_BASEURL + "events/";
 
 // Get admin events
 const getEvents = async (token) => {
